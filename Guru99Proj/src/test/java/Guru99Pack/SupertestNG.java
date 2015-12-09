abfc1e00e0fa828a94679e922f01fef2d2d42c8f
Naresh Shewakramani <ramani_naresh@yahoo.com>
2015-12-09 12:00:01 +0530

    Created excel object

--- Guru99Proj/src/test/java/Guru99Pack/SupertestNG.java
+++ Guru99Proj/src/test/java/Guru99Pack/SupertestNG.java
@@ -8,11 +8,6 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -26,6 +21,7 @@ public class SupertestNG {
 	
 	public WebDriver driver;
 	public LoginPage objLoginPage;
+	public Excelclass objexcelsheet;
 	public HomePage objHomePage;
 	
 	@BeforeMethod
@@ -35,38 +31,13 @@ public class SupertestNG {
 		driver.get("http://demo.guru99.com/V4/");
 		Reporter.log("Application is loaded", true);
 		
-		FileInputStream fis;
-		Workbook wb;
-		Sheet sh;
-		Row rw;
-		Cell cell1;
-		String cellval1;
-		Cell cell2;
-		String cellval2;
-		
-		try {
-		fis=new FileInputStream("C:\\Documents and Settings\\Pooja T\\git\\Guru99Proj\\Guru99Proj\\TestData.xls");
-		wb=WorkbookFactory.create(fis);
-		sh=wb.getSheet("Sheet1");
-				
-		} catch (Exception e){
-			throw (e);
-		}
-		
-		try {
-		rw=sh.getRow(1);
-		cell1=rw.getCell(0);
-		cellval1=cell1.getStringCellValue();
-			
-		cell2=rw.getCell(1);
-		cellval2=cell2.getStringCellValue();
-				
-		} catch (Exception e){
-			throw (e);
-		}
+
+		objexcelsheet= new Excelclass();
+		String cel1= objexcelsheet.getRowID(1,0);
+		String cel2= objexcelsheet.getRowPass(1,1);
 		
 		objLoginPage=new LoginPage(driver);
-		objLoginPage.loginToGuru99(cellval1,cellval2);
+		objLoginPage.loginToGuru99(cel1,cel2);
 		
 		Reporter.log("User is logged in successfully", true);
 		
