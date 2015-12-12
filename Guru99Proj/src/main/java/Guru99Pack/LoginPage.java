063319055d383f990c94f169708feae228a8cea0
Naresh Shewakramani <ramani_naresh@yahoo.com>
2015-12-13 01:50:55 +0530

    dec 13 2nd commit

--- Guru99Proj/src/main/java/Guru99Pack/LoginPage.java
+++ Guru99Proj/src/main/java/Guru99Pack/LoginPage.java
@@ -1,12 +1,15 @@
 package Guru99Pack;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Reporter;
 
 public class LoginPage {
 	WebDriver driver;
-	 
+	private static Logger Log = Logger.getLogger(Logger.class.getName());
+	
 	//declaration of attributes or elements
    By loginName = By.name("uid");
    By loginPassword = By.name("password");
@@ -20,10 +23,12 @@ public class LoginPage {
    
  //login method
    public void loginToGuru99(String strUserName,String strPasword){
+	   DOMConfigurator.configure("log4j.xml");
 	   driver.findElement(loginName).sendKeys(strUserName);
 	   driver.findElement(loginPassword).sendKeys(strPasword);
 	   driver.findElement(loginbtn).click();
 	   Reporter.log("User is logged in successfully",true);
+	   Log.info("User is logged in successfully");
       }
    
      
